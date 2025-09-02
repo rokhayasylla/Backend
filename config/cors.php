501f5da0fdf806776ef0ebfb7596010a83c133d5
Madisco66 <diallomadina222@gmail.com>
2025-09-02 15:23:43 +0000

    mintégration railway

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:4200','https://appboulangeriebackend-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
