9596c3f44831b6b476df0165ee59bdc262122489
rokhayasylla <syllarokhaya72@gmail.com>
2025-11-10 23:51:18 +0000

    first commit

--- database/migrations/2025_10_11_203318_create_users_table.php
+++ database/migrations/2025_10_11_203318_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
-            $table->enum('role', ['admin', 'employee', 'client'])->default('client');
+            $table->enum('role', ['admin', 'employee', 'client', 'livreur'])->default('client');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_active')->default(true);
