332b74795505ca4af4aacba127229404b4bb27fa
rokhayasylla <syllarokhaya72@gmail.com>
2025-11-22 21:47:19 +0000

    Add imageUrl1

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'storage/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200','https://memoire-frontend-nu.vercel.app'],
+    'allowed_origins' => ['http://localhost:4200', 'https://memoire-frontend-nu.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
